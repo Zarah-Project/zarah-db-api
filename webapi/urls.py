37d2e20bdbfe9cef7e9215783d751e6690b671ab
József Gábor Bóné <jozsef.bone@gmail.com>
2021-01-16 00:28:25 +0100

    feat(FileView): Add fileView.

--- webapi/urls.py
+++ webapi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from webapi.views import DocumentPublicDetail
+from webapi.views import DocumentPublicDetail, StoredFilePublicView
 
 app_name = 'webapi'
 
 urlpatterns = [
     path('document/<int:pk>/', DocumentPublicDetail.as_view(), name='document-detail'),
+
+    path('view_file/<str:id>', StoredFilePublicView.as_view(), name='stored-file-pubic-view'),
 ]
--- webapi/views.py
+++ webapi/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from django.core.exceptions import ObjectDoesNotExist
+from django_drf_filepond.api import get_stored_upload, get_stored_upload_file_data
+from rest_framework import generics, status
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from document.models import Document
+from document.models import Document, DocumentFile
 from webapi.serializers import DocumentReadPublicSerializer, DocumentReadTeamSerializer, DocumentReadIndividualSerializer
 
 
@@ -18,3 +22,34 @@ class DocumentPublicDetail(generics.RetrieveAPIView):
             return DocumentReadTeamSerializer
         if document.record_type == 'individual':
             return DocumentReadIndividualSerializer
+
+
+class StoredFilePublicView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, id):
+        restricted = False
+
+        try:
+            document_file = DocumentFile.objects.get(file_id=id)
+            attachmemnt_type = document_file.document.attachment_type
+
+            if attachmemnt_type != 'default':
+                restricted = True
+        except ObjectDoesNotExist:
+            restricted = False
+
+        if restricted:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        else:
+            try:
+                su = get_stored_upload(id)
+                (filename, bytes_io) = get_stored_upload_file_data(su)
+                response = Response()
+                response.content = bytes_io
+                response["Content-Disposition"] = "attachment; filename={0}".format(filename)
+                response["Content-Type"] = 'application/pdf'
+                return response
+            except FileNotFoundError:
+                return Response(data='Not found', status=status.HTTP_404_NOT_FOUND)
