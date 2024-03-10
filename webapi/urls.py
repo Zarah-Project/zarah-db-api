f8d35cd859dd9581f79c6e1cc3c6dd61a027b206
Josh <jozsef.bone@gmail.com>
2024-03-10 20:52:59 +0100

    Update urls.py

--- webapi/urls.py
+++ webapi/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
 
     path('document-by/<str:type>/<int:pk>', DocumentsBySpecialRecord.as_view(), name='document-by-special-record'),
 
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
